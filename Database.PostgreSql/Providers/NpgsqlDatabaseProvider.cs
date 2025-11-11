654fe70e0a6f35b29d677b9c8ff842051a1c834a
Eliseu Batista <44680425+eliseubatista99@users.noreply.github.com>
2025-11-11 09:09:47 +0000

    + Add [Database]: Created postgreSql database package (#1)

---
+++ Database.PostgreSql/Providers/NpgsqlDatabaseProvider.cs
@@ -0,0 +1,123 @@
+﻿using Database.PostgreSql.Helpers;
+using Npgsql;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Database.PostgreSql.Providers
+{
+    [ExcludeFromCodeCoverage]
+    public class NpgsqlDatabaseProvider<T>
+    {
+        public NpgsqlDatabaseProvider()
+        {
+
+        }
+
+        protected virtual string GetConnectionString()
+        {
+            return string.Empty;
+        }
+
+        protected List<T> ExecuteReadMultiple(string command)
+        {
+            var connectionString = GetConnectionString();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                List<T> result = new List<T>();
+
+                connection.Open();
+
+                var (transaction, commandExecutor) = NpgsqlDatabaseHelper.InitialzieSqlTransaction(connection);
+
+                try
+                {
+                    commandExecutor.CommandText = command;
+
+                    using (var sqlReader = commandExecutor.ExecuteReader())
+                    {
+                        while (sqlReader!.Read())
+                        {
+                            var dataEntry = GetObjectFromDataReader(sqlReader)!;
+
+                            result.Add(dataEntry);
+                        }
+                    }
+
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        protected T? ExecuteRead(string command)
+        {
+            var connectionString = GetConnectionString();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                T? result = default;
+
+                connection.Open();
+
+                var (transaction, commandExecutor) = NpgsqlDatabaseHelper.InitialzieSqlTransaction(connection);
+
+                try
+                {
+                    commandExecutor.CommandText = command;
+
+                    var sqlReader = commandExecutor.ExecuteReader();
+
+                    if (sqlReader.HasRows)
+                    {
+                        sqlReader.Read();
+                        result = GetObjectFromDataReader(sqlReader);
+
+                    }
+
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        protected bool ExecuteWrite(string command)
+        {
+            var connectionString = GetConnectionString();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var (transaction, commandExecutor) = NpgsqlDatabaseHelper.InitialzieSqlTransaction(connection);
+
+                try
+                {
+                    commandExecutor.CommandText = command;
+
+                    commandExecutor.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        protected virtual T? GetObjectFromDataReader(NpgsqlDataReader dataReader)
+        {
+            return default(T);
+        }
+    }
+
+}
