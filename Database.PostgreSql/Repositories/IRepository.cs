8a574ec6c3ecc8b36781ff9bac4375c8d9df1a92
Eliseu Batista <eliseubatista99@outlook.com>
2026-01-07 12:39:27 +0000

    improv

--- Database.PostgreSql/Repositories/IRepository.cs
+++ Database.PostgreSql/Repositories/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,17 @@ namespace Database.PostgreSql.Repositories
     public interface IRepository<T> where T : class
     {
         IQueryable<T> Query(); // To build queries
+
         Task<T?> GetByIdAsync(string id);
+
         Task<bool> AddAsync(T entity);
+
         void Update(T entity);
+
         void Delete(T entity);
+
+        public Task<int> DeleteAsync(Expression<Func<T, bool>> predicate);
+
         Task<int> SaveChangesAsync();
     }
 }
